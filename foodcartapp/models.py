e7a1736604aad68560009f7854ef977cd3edca8e
GenmaHimmuro <t.suleimanov2@gmail.com>
2025-10-11 18:34:39 +0500

    Added comment field (displayed in the manager's window)

--- foodcartapp/models.py
+++ foodcartapp/models.py
@@ -158,6 +158,12 @@ class Order(models.Model):
         verbose_name='Статус заказа',
         db_index=True,
     )
+    comment = models.TextField(
+        max_length=300,
+        verbose_name='Комментарий к заказу',
+        blank=True,
+    )
+
     class Meta:
         verbose_name = 'заказ'
         verbose_name_plural = 'заказы'
