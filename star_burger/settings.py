a5cd3987f4f449f22c056b54ba59efddac4e27ef
GenmaHimmuro <t.suleimanov2@gmail.com>
2025-12-06 19:46:45 +0500

    Update settings.py

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -127,7 +127,7 @@ STATICFILES_DIRS = [
 
 
 ROLLBAR = {
-    'access_token': env.str('ROLLBAR_ACCESS_TOKEN'),
+    'access_token': env('ROLLBAR_ACCESS_TOKEN'),
     'environment': 'development' if DEBUG else 'production',
     'code_version': '1.0',
     'root': BASE_DIR,
